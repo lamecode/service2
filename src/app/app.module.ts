a32bd271e08ad5afab8121922a9bd49d1066f8be
lamecode <https://github.com/DreamTeamAccomplisher>
2019-12-16 23:23:38 +0200

    prev options div

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPGComponent } from './main-pg/main-pg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPGComponent
   ],
   imports: [
     BrowserModule,
